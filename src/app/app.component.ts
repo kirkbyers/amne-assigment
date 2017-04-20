14aff700636bfdff2c8d425c8bfe4d8109661040
Kirk byers <kingbyers@gmail.com>
2017-04-19 22:32:13 -0500

    Remove broken progress bar.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
@@ -7,17 +7,20 @@ import { GooglePlacesService } from './google-places.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent implements OnInit {
   searchFormGroup: FormGroup;
   searchResults$ = new BehaviorSubject(null);
+  progress$ = new BehaviorSubject(100);
 
   constructor (
     private _googlePlacesService: GooglePlacesService,
     private _formBuilder: FormBuilder
   ) {
     this.searchResults$.subscribe(val => console.log(val));
+    this.progress$.subscribe(val => console.log(val));
   }
 
   ngOnInit () {
@@ -28,8 +31,12 @@ export class AppComponent implements OnInit {
   }
 
   onSearch (updatedFormGroup: FormGroup) {
+    this.progress$.next(0);
     const formGroupValue = updatedFormGroup.value;
     this._googlePlacesService.getNearestFrom2(formGroupValue.addressOne, formGroupValue.addressTwo)
-      .subscribe(result => this.searchResults$.next(result));
+      .subscribe(result => {
+        this.progress$.next(100);
+        this.searchResults$.next(result)
+      });
   }
 }
